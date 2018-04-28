792f63a7efeaf5994c966aada2a472f5317fe4d3
Barbara Washington <barbara.m.washington@gmail.com>
2018-04-28 03:43:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,11 +23,18 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do | win_combination |
-    location1 = win_combination[0]
-    location2 = win_combination[1]
-    location3 = win_combination[2]
-    board(location1) == board(location2) && board(location2) == board(location3) && board(location1) != " "
-  end
+  
 end  
+
+
+
+
+# def won?(board)
+#   WIN_COMBINATIONS.detect do | win_combination |
+#     location1 = win_combination[0]
+#     location2 = win_combination[1]
+#     location3 = win_combination[2]
+#     board(location1) == board(location2) && board(location2) == board(location3) && board(location1) != " "
+#   end
+# end  
 
