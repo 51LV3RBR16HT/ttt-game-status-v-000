e10ad15a022452b6a2f63628562c5230a8174b02
Barbara Washington <barbara.m.washington@gmail.com>
2018-04-28 01:50:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,5 +24,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   #return a winning combination, or return false
-  WIN_COMBINATIONS.each? do |
+  WIN_COMBINATIONS.each? do | winning_combinations |
+    winning_combinations
 end  
